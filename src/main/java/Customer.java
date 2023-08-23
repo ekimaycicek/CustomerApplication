6f818198555445b4720331ddd1f96efa75d1ca8d
ekimaycicek <ekimaycicek@icloud.com>
2023-08-23 17:00:42 +0300

    new

---
+++ src/main/java/Customer.java
@@ -0,0 +1,35 @@
+public class Customer {
+
+    /*
+Müşteri yönetim uygulaması ile müşterilerin adını, soyadını ve e-posta adresini
+takip etmeniz gerekiyor.
+ */
+
+
+
+
+
+    private String name;
+    private String surname;
+    private String email;
+
+    public Customer(String name, String surname, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+}
