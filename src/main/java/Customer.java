2624a16339d67fa4acd117e21a81a18a1f8d46e7
ekimaycicek <ekimaycicek@icloud.com>
2023-12-09 19:00:19 +0300

    new

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -13,6 +13,7 @@ takip etmeniz gerekiyor.
     private String surname;
     private String email;
 
+
     public Customer(String name, String surname, String email) {
         this.name = name;
         this.surname = surname;
